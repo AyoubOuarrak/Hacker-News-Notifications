f30fac08e4d14bc9d9b51cf2709321a3c4bc867b
Ayoub Ouarrak <Ayoub@admins-macbook-pro-3.local>
2016-05-28 19:09:15 +0100

    Initial Commit

---
+++ Hacker News Notification/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Hacker News Notification
+//
+//  Created by Ayoub Ouarrak on 28/05/2016.
+//  Copyright © 2016 Ayoub Ouarrak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hacker News NotificationTests/Hacker_News_NotificationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hacker_News_NotificationTests.swift
+//  Hacker News NotificationTests
+//
+//  Created by Ayoub Ouarrak on 28/05/2016.
+//  Copyright © 2016 Ayoub Ouarrak. All rights reserved.
+//
+
+import XCTest
+@testable import Hacker_News_Notification
+
+class Hacker_News_NotificationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
